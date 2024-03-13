2391c86b9e4b4a75ec1d536a3a0e5dfcc8e89066
yeesem <yeesemoon2002gmail.com>
2024-03-14 00:09:15 +0800

    commit

--- MainPage.py
+++ MainPage.py
@@ -6,6 +6,7 @@ import datetime
 import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 from IncomeStatement import Income_Statement
+from KLSEScraping import KLSE
 
 
 ###SIDEBAR
@@ -20,9 +21,25 @@ def menu():
 menu = menu()
 
 #User input stock symbol
-key_stock_symbol = "1155.KL"
-stockName = st.text_input("Stock Symbol",key = key_stock_symbol)
-ticker = yf.Ticker(stockName)
+key_stock_symbol = "1155"
+stockName = st.text_input("Stock Symbol",key = key_stock_symbol,value = key_stock_symbol)
+ticker = yf.Ticker(stockName + ".KL")
+
+#Call income statement class
+IncomeStatement = Income_Statement(ticker)
+  
+#Call KLSEScraping class
+klse = KLSE(stockName)
+stock_basic_info = klse.get_stock_basic_info()
+
+short_name = stock_basic_info["short name"]
+symbol = stock_basic_info["symbol"]
+price = stock_basic_info["price"]
+percentage_difference = stock_basic_info["different price in percentage"]
+full_name = stock_basic_info["full name"]
+
+# Define the space between elements
+space = "&nbsp;" * 10
 
 ###MAIN PAGE
 if menu == 'Main Page':
@@ -42,17 +59,25 @@ if menu == 'Main Page':
   
   #Diaplay timeline side bar
   df = timeline()
-
-#   #User input stock symbol
-#   key_stock_symbol = "1155.KL"
-#   stockName = st.text_input("Stock Symbol",key = key_stock_symbol)
-#   ticker = yf.Ticker(stockName)
-
-  #Call income statement class
-  IncomeStatement = Income_Statement(ticker)
-  
   
   historyPriceData = ticker.history(period="1mon",start = df["Start Date"],end = df["End Date"])
+
+  flex_style = "display: flex; justify-content: space-between;"
+
+  # Determine the color based on the value of percentage_difference
+  color = "green" if float(percentage_difference.strip('%')) > 0 else "red"
+
+  st.write(f'<div style="{flex_style}; font-size: 35px;">'
+         f'<span style="font-weight: bold;">{short_name} {symbol}</span>'
+         f'<div style="text-align: right; font-size: 30px; color: {color};">'
+         f'<span style="font-weight: bold;">{price} ({percentage_difference})</span>'
+         f'</div></div>',
+         unsafe_allow_html=True)
+
+  st.write("### " + full_name)
+
+  st.markdown(stock_basic_info["company summary"])
+
 
   st.write("#### Technical Chart - ",stockName)
   st.line_chart(historyPriceData.Close)
@@ -62,6 +87,11 @@ if menu == 'Main Page':
 
 ###INCOME STATEMENT PAGE
 if menu == 'Income Statement':
+   flex_style = "display: flex; justify-content: space-between;"
+   st.write(f'<div style="{flex_style}; font-size: 35px;">'
+           f'<span style="font-weight: bold;">{full_name}</span>'
+           f'</div>',
+         unsafe_allow_html=True)
    st.write("### Income Statement")   
    st.write(st.session_state.IncomeStatement.ShowIncomeStatement())
 
