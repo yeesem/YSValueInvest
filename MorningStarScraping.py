ccbe4d66ac845526a1ddb98a9bf78be20ab2c3b1
yeesem <yeesemoon2002gmail.com>
2024-03-16 01:30:44 +0800

    commit

--- MorningStarScraping.py
+++ MorningStarScraping.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 cService = webdriver.ChromeService(executable_path='C:\\Users\\OON YEE SEM\\Documents\\Developer\\Chrome Driver\\chromedriver.exe')
@@ -9,5 +10,7 @@ browser.get("https://www.morningstar.com/stocks/xkls/5196/Valuation")
 
 time.sleep(2)
 
-element = browser.find_element("class","sal-component-title-h2")
-print(element)
+elements = browser.find_elements(By.CSS_SELECTOR, ".mds-td__sal")
+
+texts = [element.text.strip() for element in elements]
+print(texts)
